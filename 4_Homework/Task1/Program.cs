040722b64fe3d63e49f571dc2fa15947841528f7
Alexandr <headofthefather@gmail.com>
2022-12-30 03:25:21 +0300

    4_Homework

---
+++ 4_Homework/Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Enter the number A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the number B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int temp = a;
+    if (b == 0) 
+    {
+        Console.WriteLine("The answer is: 1"); 
+    }
+
+    else
+    {
+        for(int i = 1; i < b; i++) 
+    {
+        a *= temp;
+    }
+    Console.WriteLine("The answer is: ");
+    Console.WriteLine(a);
+    }
---
+++ 4_Homework/Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Enter the number");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+    for (int i = num ; i > 0 ; i/= 10)
+{
+     int temp = i % 10;
+     if (temp > 0)
+     {
+        if (num % temp == 0)
+        {
+            sum = sum + temp;
+        }
+     }
+}
+Console.WriteLine(sum);
